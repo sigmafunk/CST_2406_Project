d020aa9346983c472af9b5902a757b1561cd107b
KAUNDA HUGHES <sigmafunk5417@gmail.com>
2016-04-21 18:21:26 -0400

    Added files via upload

---
+++ Index.php
@@ -0,0 +1,43 @@
+Index.php
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link rel="stylesheet" href="Style.css" type="text/css"/>
+<title>Home Page</title>
+
+</head>
+<body> 
+<table width="100%" height="100%" 
+> <tr height="15%"> 
+<td><?php include 'Header.php';?></td> 
+</tr> 
+<tr height="80%"> 
+<td align="center" valign="baseline"><table width="70%"> 
+<tr height="10%"> 
+	<td align="center"><a href="index.php" class="stylelink" style="text- 
+	decoration:none">Home</a></td> 
+<td align="right"><a href="Student_login.php" class="stylelink" 
+style="text-decoration:none">Student Login</a> 
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+<a href="Admin_login.php" class="stylelink" style="text- 
+decoration:none">Administrator Login</a></td> 
+</tr> 
+<tr> 
+<td>&nbsp;</td> 
+<td>&nbsp;</td> 
+</tr> 
+<tr> 
+<td>&nbsp;</td> 
+<td>&nbsp;</td> 
+</tr> 
+</table></td> 
+</tr> 
+<tr height="5%"> 
+<td><?php include 'Footer.php';?></td> 
+</tr> 
+</table> 
+</body> 
+</html>
+
+
